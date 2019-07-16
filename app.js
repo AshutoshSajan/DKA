a579bd234fcd91987e7fe0b47f1e1dd512f60c21
sajan <sajanloveu123@gmail.com>
2019-07-16 10:49:10 +0530

    change app.js static folder path

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/client/public')));
 
 app.use('/dist/bundle', expressStaticGzip(path.join(__dirname, 'dist/bundle'), {
   enableBrotli: true,
