7bd46b3e735801d3c72ff1f63c3fd71a7bbcd8f9
sajan <sajanloveu123@gmail.com>
2019-07-18 14:44:51 +0530

    add auto login validation check

--- client/src/App.js
+++ client/src/App.js
@@ -16,18 +16,26 @@ import PrivateRoute from './components/PrivateRoute';
 class App extends Component {
 
 	componentDidMount() {
-    axios.post('http://localhost:3000/api/v1/users/me', { headers: {"Authorization" : localStorage.getItem("jwt") }})
-    .then((res) => {
-      console.log(res, "login data");
-      if(res.data.success){
-        console.log(data, "/me response");
-        this.props.dispatch({ type: "USER_LOGIN_SUCCESS", data: res.data });
-        this.props.history.push('/');
-      }
-    })
-    .catch(function (error) {
-      console.log(error, "catch error");
-    });
+    var token = localStorage.getItem("jwt") || "";
+    if(token){
+      axios.post('http://localhost:3000/api/v1/users/me',
+        {
+          headers: {"Authorization" : token }
+        })
+      .then((res) => {
+        console.log(res, "login data");
+        if(res.data.success){
+          console.log(data, "/me response");
+          this.props.dispatch({ type: "USER_LOGIN_SUCCESS", data: res.data });
+          this.props.history.push('/');
+        }
+      })
+      .catch(function (error) {
+        console.log(error, "catch error");
+      });
+    } else {
+      this.this.props.history.push("/users/login")
+    }
 	}
 
   render() {
