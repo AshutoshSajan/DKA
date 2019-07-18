63ca1195f4837735e2a6c939aad91d5f6c072f73
sajan <sajanloveu123@gmail.com>
2019-07-18 22:44:47 +0530

    add registration form styling

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -3,79 +3,135 @@ import React, { Component } from 'react';
 class Register extends Component {
 	render() {
 		return (
-			<div className="container" style={{ border: "1px solid rgba(0,0,0,0.2)", padding: "20px", borderRadius:'4px' }}>
+			<div className="container" style={{ border: "1px solid rgba(0,0,0,0.2)", padding: "40px 60px", borderRadius:'4px', width: "800px"}}>
 				<div style={{ display: "flex" , justifyContent:'space-between', flexWrap:'wrap'}}>
 					<div className="field">
-					  <p className="label">First Name</p>
-					  <div className="control">
-					    <input className="input" type="text" placeholder="Text input"/>
-					  </div>
+					  <div className="field">
+						  <p className="label">First Name</p>
+						  <div className="control has-icons-left has-icons-right">
+						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <span className="icon is-small is-left">
+						      <i className="fas fa-user"></i>
+						    </span>
+						    <span className="icon is-small is-right">
+						      <i className="fas fa-check"></i>
+						    </span>
+						  </div>
+						  <p className="help is-success">This username is available</p>
+						</div>
 					</div>
 
 					<div className="field">
-					  <p className="label">Last Name</p>
-					  <div className="control">
-					    <input className="input" type="text" placeholder="Text input"/>
-					  </div>
+					  <div className="field">
+						  <p className="label">Last Name</p>
+						  <div className="control has-icons-left has-icons-right">
+						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <span className="icon is-small is-left">
+						      <i className="fas fa-user"></i>
+						    </span>
+						    <span className="icon is-small is-right">
+						      <i className="fas fa-check"></i>
+						    </span>
+						  </div>
+						  <p className="help is-success">This username is available</p>
+						</div>
 					</div>
 
 					<div className="field">
-					  <p className="label">Mothers Name</p>
-					  <div className="control">
-					    <input className="input" type="text" placeholder="Text input"/>
-					  </div>
+					  <div className="field">
+						  <p className="label">Mothers Name</p>
+						  <div className="control has-icons-left has-icons-right">
+						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <span className="icon is-small is-left">
+						      <i className="fas fa-user"></i>
+						    </span>
+						    <span className="icon is-small is-right">
+						      <i className="fas fa-check"></i>
+						    </span>
+						  </div>
+						  <p className="help is-success">This username is available</p>
+						</div>
 					</div>
 
 					<div className="field">
-					  <p className="label">Permanent Address</p>
-					  <div className="control">
-					    <input className="input" type="text" placeholder="Text input"/>
-					  </div>
+					  <div className="field">
+						  <p className="label">Permanent Address</p>
+						  <div className="control has-icons-left has-icons-right">
+						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <span className="icon is-small is-left">
+						      <i className="fas fa-user"></i>
+						    </span>
+						    <span className="icon is-small is-right">
+						      <i className="fas fa-check"></i>
+						    </span>
+						  </div>
+						  <p className="help is-success">This username is available</p>
+						</div>
 					</div>
 
 					<div className="field">
-					  <p className="label">Current Address</p>
-					  <div className="control">
-					    <input className="input" type="text" placeholder="Text input"/>
-					  </div>
+					  <div className="field">
+						  <p className="label">Current Address</p>
+						  <div className="control has-icons-left has-icons-right">
+						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <span className="icon is-small is-left">
+						      <i className="fas fa-user"></i>
+						    </span>
+						    <span className="icon is-small is-right">
+						      <i className="fas fa-check"></i>
+						    </span>
+						  </div>
+						  <p className="help is-success">This username is available</p>
+						</div>
 					</div>
 					
 					<div className="field">
-					  <p className="label">Phone</p>
-					  <div className="control">
-					    <input className="input" type="text" placeholder="Text input"/>
-					  </div>
+					  <div className="field">
+						  <p className="label">Phone</p>
+						  <div className="control has-icons-left has-icons-right">
+						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <span className="icon is-small is-left">
+						      <i className="fas fa-user"></i>
+						    </span>
+						    <span className="icon is-small is-right">
+						      <i className="fas fa-check"></i>
+						    </span>
+						  </div>
+						  <p className="help is-success">This username is available</p>
+						</div>
 					</div>
 
 					<div className="field">
-					  <p className="label">DOB</p>
-					  <div className="control">
-					    <input className="input" type="text" placeholder="Text input"/>
-					  </div>
+					  <div className="field">
+						  <p className="label">DOB</p>
+						  <div className="control has-icons-left has-icons-right">
+						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <span className="icon is-small is-left">
+						      <i className="fas fa-user"></i>
+						    </span>
+						    <span className="icon is-small is-right">
+						      <i className="fas fa-check"></i>
+						    </span>
+						  </div>
+						  <p className="help is-success">This username is available</p>
+						</div>
 					</div>
 
 					<div className="field">
-					  <p className="label">Pin</p>
-					  <div className="control">
-					    <input className="input" type="text" placeholder="Text input"/>
-					  </div>
+					  <div className="field">
+						  <p className="label">PIN</p>
+						  <div className="control has-icons-left has-icons-right">
+						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <span className="icon is-small is-left">
+						      <i className="fas fa-user"></i>
+						    </span>
+						    <span className="icon is-small is-right">
+						      <i className="fas fa-check"></i>
+						    </span>
+						  </div>
+						  <p className="help is-success">This username is available</p>
+						</div>
 					</div>
-
-					
-				</div>
-
-				<div className="field">
-				  <p className="label">Last Name</p>
-				  <div className="control has-icons-left has-icons-right">
-				    <input className="input is-success" type="text" placeholder="Text input" value=""/>
-				    <span className="icon is-small is-left">
-				      <i className="fas fa-user"></i>
-				    </span>
-				    <span className="icon is-small is-right">
-				      <i className="fas fa-check"></i>
-				    </span>
-				  </div>
-				  <p className="help is-success">This username is available</p>
 				</div>
 
 				<div className="field">
@@ -105,9 +161,9 @@ class Register extends Component {
 				</div>
 
 				<div className="field">
-				  <p className="label">Message</p>
+				  <p className="label">Why Karate</p>
 				  <div className="control">
-				    <textarea className="textarea" placeholder="Textarea"></textarea>
+				    <textarea className="textarea" placeholder="Please tell us why you want to learn Karate"></textarea>
 				  </div>
 				</div>
 
