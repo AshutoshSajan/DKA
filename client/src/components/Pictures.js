e01b2a30c3237545640c902a7273536c19961bd6
sajan <sajanloveu123@gmail.com>
2019-07-19 14:17:26 +0530

    add register form handle change fn

--- client/src/components/Pictures.js
+++ client/src/components/Pictures.js
@@ -4,30 +4,30 @@ class Pictures extends React.Component {
   
   render() {
     return (
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img className="hero-image" style={{ objectPosition: "50% -100%" }} src="http://zanshinmkd.com/wp-content/uploads/2012/03/karate-moves.jpg" class="d-block w-100" alt="..."/>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="hero-image" style={{ objectPosition: "50% -100%" }} src="http://zanshinmkd.com/wp-content/uploads/2012/03/karate-moves.jpg" className="d-block w-100" alt="..."/>
           </div>
-          <div class="carousel-item">
-            <img className="hero-image" style={{ objectPosition: "50% -50%" }} src="https://www.wikihow.com/images/7/77/Block-Punches-in-Karate-Step-13.jpg" class="d-block w-100" alt="..."/>
+          <div className="carousel-item">
+            <img className="hero-image" style={{ objectPosition: "50% -50%" }} src="https://www.wikihow.com/images/7/77/Block-Punches-in-Karate-Step-13.jpg" className="d-block w-100" alt="..."/>
           </div>
-          <div class="carousel-item">
-            <img className="hero-image" style={{ objectPosition: "50% -50%" }} src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.QZ31Q49skztB21jz8lhfowHaEY%26pid%3DApi&f=1" class="d-block w-100" alt="..."/>
+          <div className="carousel-item">
+            <img className="hero-image" style={{ objectPosition: "50% -50%" }} src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.QZ31Q49skztB21jz8lhfowHaEY%26pid%3DApi&f=1" className="d-block w-100" alt="..."/>
           </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
         </a>
       </div>
     )
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+
+	state = {
+		user: {},
+		error: {},
+		terms: false
+	}
+
+	handleChange = (e) => {
+		console.log(e.target.value, "handleChange fired");
+		const { name , value } = e.target;
+		this.setState({ [name]: value });
+	}
+
 	render() {
 		return (
 			<div className="container" style={{ border: "1px solid rgba(0,0,0,0.2)", padding: "2.5rem 4rem", borderRadius:'4px', width: "50%"}}>
+				<div style={{display:'flex', alignItems:'center', flexWrap:'wrap'}}>
+					<img src="/dka.jpeg" alt="logo" height="50" width="50"/>
+					<p>Wellcome to Dhauladhar Karate Acedemy plese fill all the feilds to join us</p>
+				</div>
 				<div style={{ display: "flex" , justifyContent:'space-between', flexWrap:'wrap'}}>
 					<div className="field">
 					  <div className="field">
 						  <p className="label">First Name</p>
 						  <div className="control has-icons-left has-icons-right">
-						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <input className="input is-success" type="text" placeholder="First Name" name="firstName" value={ this.state.firstName } onChange={this.handleChange} required />
 						    <span className="icon is-small is-left">
 						      <i className="fas fa-user"></i>
 						    </span>
@@ -17,7 +34,7 @@ class Register extends Component {
 						      <i className="fas fa-check"></i>
 						    </span>
 						  </div>
-						  <p className="help is-success">This username is available</p>
+						  <p className="help is-success">{this.state.error.username}</p>
 						</div>
 					</div>
 
@@ -25,7 +42,7 @@ class Register extends Component {
 					  <div className="field">
 						  <p className="label">Last Name</p>
 						  <div className="control has-icons-left has-icons-right">
-						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <input className="input is-success" type="text" placeholder="Last Name" name="lastName" value={ this.state.lastName } onChange={this.handleChange} />
 						    <span className="icon is-small is-left">
 						      <i className="fas fa-user"></i>
 						    </span>
@@ -33,7 +50,7 @@ class Register extends Component {
 						      <i className="fas fa-check"></i>
 						    </span>
 						  </div>
-						  <p className="help is-success">This username is available</p>
+						  <p className="help is-success">{this.state.error.lastName}</p>
 						</div>
 					</div>
 
@@ -41,7 +58,7 @@ class Register extends Component {
 					  <div className="field">
 						  <p className="label">Mothers Name</p>
 						  <div className="control has-icons-left has-icons-right">
-						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <input className="input is-success" type="text" placeholder="Mothers Name" name="mothersName" value={ this.state.mothersName } onChange={this.handleChange}/>
 						    <span className="icon is-small is-left">
 						      <i className="fas fa-user"></i>
 						    </span>
@@ -49,7 +66,7 @@ class Register extends Component {
 						      <i className="fas fa-check"></i>
 						    </span>
 						  </div>
-						  <p className="help is-success">This username is available</p>
+						  <p className="help is-success">{this.state.error.mothersName}</p>
 						</div>
 					</div>
 
@@ -57,7 +74,7 @@ class Register extends Component {
 					  <div className="field">
 						  <p className="label">Permanent Address</p>
 						  <div className="control has-icons-left has-icons-right">
-						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <input className="input is-success" type="text" placeholder="VPO-Dist-state" name="address" value={ this.state.address } onChange={this.handleChange} required />
 						    <span className="icon is-small is-left">
 						      <i className="fas fa-user"></i>
 						    </span>
@@ -65,7 +82,7 @@ class Register extends Component {
 						      <i className="fas fa-check"></i>
 						    </span>
 						  </div>
-						  <p className="help is-success">This username is available</p>
+						  <p className="help is-success">{this.state.error.address}</p>
 						</div>
 					</div>
 
@@ -73,7 +90,7 @@ class Register extends Component {
 					  <div className="field">
 						  <p className="label">Current Address</p>
 						  <div className="control has-icons-left has-icons-right">
-						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <input className="input is-success" type="text" placeholder="VPO-Dist-state" name="currentAddress" value={ this.state.currentAddress } onChange={this.handleChange} required />
 						    <span className="icon is-small is-left">
 						      <i className="fas fa-user"></i>
 						    </span>
@@ -81,7 +98,7 @@ class Register extends Component {
 						      <i className="fas fa-check"></i>
 						    </span>
 						  </div>
-						  <p className="help is-success">This username is available</p>
+						  <p className="help is-success">{this.state.error.currentAddress}</p>
 						</div>
 					</div>
 					
@@ -89,7 +106,7 @@ class Register extends Component {
 					  <div className="field">
 						  <p className="label">Phone</p>
 						  <div className="control has-icons-left has-icons-right">
-						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <input className="input is-success" type="text" placeholder="Mobile number" name="phone" value={ this.state.phone } onChange={this.handleChange} required />
 						    <span className="icon is-small is-left">
 						      <i className="fas fa-user"></i>
 						    </span>
@@ -97,7 +114,7 @@ class Register extends Component {
 						      <i className="fas fa-check"></i>
 						    </span>
 						  </div>
-						  <p className="help is-success">This username is available</p>
+						  <p className="help is-success">{this.state.error.phone}</p>
 						</div>
 					</div>
 
@@ -105,7 +122,7 @@ class Register extends Component {
 					  <div className="field">
 						  <p className="label">DOB</p>
 						  <div className="control has-icons-left has-icons-right">
-						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <input className="input is-success" type="date" placeholder="DOB" name="dob" value={ this.state.dob } onChange={this.handleChange} required />
 						    <span className="icon is-small is-left">
 						      <i className="fas fa-user"></i>
 						    </span>
@@ -113,7 +130,7 @@ class Register extends Component {
 						      <i className="fas fa-check"></i>
 						    </span>
 						  </div>
-						  <p className="help is-success">This username is available</p>
+						  <p className="help is-success">{this.state.error.dob}</p>
 						</div>
 					</div>
 
@@ -121,7 +138,7 @@ class Register extends Component {
 					  <div className="field">
 						  <p className="label">PIN</p>
 						  <div className="control has-icons-left has-icons-right">
-						    <input className="input is-success" type="text" placeholder="Text input" value=""/>
+						    <input className="input is-success" type="number" placeholder="PIN/zip code" name="pin" value={ this.state.pin } onChange={this.handleChange} required />
 						    <span className="icon is-small is-left">
 						      <i className="fas fa-user"></i>
 						    </span>
@@ -129,7 +146,7 @@ class Register extends Component {
 						      <i className="fas fa-check"></i>
 						    </span>
 						  </div>
-						  <p className="help is-success">This username is available</p>
+						  <p className="help is-success">{this.state.error.pin}</p>
 						</div>
 					</div>
 				</div>
@@ -137,7 +154,7 @@ class Register extends Component {
 				<div className="field">
 				  <p className="label">Email</p>
 				  <div className="control has-icons-left has-icons-right">
-				    <input className="input is-danger" type="email" placeholder="Email input" value=""/>
+				    <input className="input is-danger" type="email" placeholder="example@gmail.com" name="email" value={ this.state.email } onChange={this.handleChange} required/>
 				    <span className="icon is-small is-left">
 				      <i className="fas fa-envelope"></i>
 				    </span>
@@ -145,25 +162,50 @@ class Register extends Component {
 				      <i className="fas fa-exclamation-triangle"></i>
 				    </span>
 				  </div>
-				  <p className="help is-danger">This email is invalid</p>
+				  <p className="help is-danger">{this.state.error.email}</p>
 				</div>
 
-				<div className="field">
-				  <p className="p">Subject</p>
-				  <div className="control">
-				    <div className="select">
-				      <select>
-				        <option>Select dropdown</option>
-				        <option>With options</option>
-				      </select>
-				    </div>
-				  </div>
+				<div style={{display:"flex" }}>
+					<div className="field">
+					  <p className="p">Weight</p>
+					  <div className="control">
+					    <div className="select" style={{marginRight:'20px'}}>
+					      <select name="weight" onChange={this.handleChange}>
+					        <option>30-35</option>
+					        <option>35-40</option>
+					        <option>40-45</option>
+					        <option>45-50</option>
+					        <option>50-55</option>
+					        <option>55-60</option>
+					        <option>60-65</option>
+					        <option>65-70</option>
+					        <option>70-75</option>
+					        <option>75-80</option>
+					      </select>
+					    </div>
+					  </div>
+					</div>
+				
+
+					<div className="field">
+					  <p className="p">What you do ?</p>
+					  <div className="control">
+					    <div className="select">
+					      <select name="job" onChange={this.handleChange}>
+					        <option>Student</option>
+					        <option>Buisnessman</option>
+					        <option>Govt. employe</option>
+					        <option>Teacher</option>
+					      </select>
+					    </div>
+					  </div>
+					</div>
 				</div>
 
 				<div className="field">
 				  <p className="label">Why Karate</p>
 				  <div className="control">
-				    <textarea className="textarea" placeholder="Please tell us why you want to learn Karate"></textarea>
+				    <textarea className="textarea" placeholder="Please tell us why you want to learn Karate ?"></textarea>
 				  </div>
 				</div>
 
@@ -179,11 +221,11 @@ class Register extends Component {
 				<div className="field">
 				  <div className="control">
 				    <p className="radio">
-				      <input type="radio" name="question"/>
+				      <input type="radio" name="terms" value={this.state.terms} onChange={this.handleChange} />
 				      Yes
 				    </p>
 				    <p className="radio">
-				      <input type="radio" name="question"/>
+				      <input type="radio" name="terms" value={this.state.terms} onChange={this.handleChange} />
 				      No
 				    </p>
 				  </div>
